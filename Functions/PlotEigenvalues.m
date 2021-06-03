15675fc2971c7068489ce8bac605223a81470864
Maximiliano Gonzalez Pazo <silver.maxi7@gmail.com>
2021-06-03 17:58:01 -0300

    display

--- Functions/PlotEigenvalues.m
+++ Functions/PlotEigenvalues.m
@@ -1,13 +1,16 @@
+individual = 1;
 n = numel([dir('Images\*.jpg'); dir('Images\*.gif'); dir('Images\*.png')]);
 for i = 1:n
-    figure
     %%  Choose Image and Calculate SVD
     [I,name] = ChooseImageGS(i);
     X = SquareMatrix(I);
     X = double(X);
-    %subplot(1,2,1)
-    %imagesc(X)
-    %axis off;colormap(1 - gray());
+    if individual == 1
+        figure
+        subplot(1,2,1)
+        imagesc(X)
+        axis off;colormap(1 - gray());
+    end
     
     if isfile(strcat('Data\', name, '.mat'))
         load(strcat('Data\', name, '.mat'))
@@ -21,12 +24,17 @@ for i = 1:n
     s = mat2gray(diag(S));
     r = 0:1/ss(1):1-1/ss(1);
     in = randi([1 size(r,2)],1);
+    if individual == 1
+       subplot(1,2,2) 
+    end
     loglog(r, s, 'k','LineWidth',1, 'color', ChooseColor(name)), grid on    
     xlabel('r')
     text(r(in),s(in),strcat('\leftarrow ', name))
     ylabel('Singular value, \sigma_r')
     set(gca, 'FontSize', 14)
-    % hold on
+    if individual ~= 1
+        hold on
+    end
     
     %subplot(1,2,2)
     %plot(cumsum(diag(S))/sum(diag(S)),'k','LineWidth',2),grid on
