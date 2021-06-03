af00e67a6eea8f2c36e7708f3b9e3cf721d5d924
Maximiliano Gonzalez Pazo <silver.maxi7@gmail.com>
2021-06-03 17:47:31 -0300

    Data

--- Functions/PlotEigenvalues.m
+++ Functions/PlotEigenvalues.m
@@ -1,4 +1,6 @@
-for i = 1:13
+n = numel([dir('Images\*.jpg'); dir('Images\*.gif'); dir('Images\*.png')]);
+for i = 1:n
+    figure
     %%  Choose Image and Calculate SVD
     [I,name] = ChooseImageGS(i);
     X = SquareMatrix(I);
@@ -6,8 +8,14 @@ for i = 1:13
     %subplot(1,2,1)
     %imagesc(X)
     %axis off;colormap(1 - gray());
-    [~,S,~] = svd(X);
-
+    
+    if isfile(strcat('Data\', name, '.mat'))
+        load(strcat('Data\', name, '.mat'))
+    else 
+        [~,S,~] = svd(X);
+        save(strcat('Data\', name), 'S')
+    end
+    
     %% Singular Values
     ss = size(diag(S));
     s = mat2gray(diag(S));
@@ -18,7 +26,7 @@ for i = 1:13
     text(r(in),s(in),strcat('\leftarrow ', name))
     ylabel('Singular value, \sigma_r')
     set(gca, 'FontSize', 14)
-    hold on
+    % hold on
     
     %subplot(1,2,2)
     %plot(cumsum(diag(S))/sum(diag(S)),'k','LineWidth',2),grid on
